1bca1b55f44e85dfd526232529c1a044561b315c
sby4 <syb4.agile@gmail.com>
2016-02-12 19:22:02 +0900

    init

---
+++ VirtualTourist/SharedConstants.swift
@@ -0,0 +1,10 @@
+//
+//  SharedConstants.swift
+//  VirtualTourist
+//
+//  Created by nekki t on 2016/02/12.
+//  Copyright © 2016年 next3. All rights reserved.
+//
+
+import Foundation
+
---
+++ VirtualTourist/TopViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TopViewController.swift
+//  VirtualTourist
+//
+//  Created by nekki t on 2016/02/12.
+//  Copyright © 2016年 next3. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class TopViewController: UIViewController {
+    // UserDefaultsにロケーションがある場合は、そのロケーションを表示
+    
+}
